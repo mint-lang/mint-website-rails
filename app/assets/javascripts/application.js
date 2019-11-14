55e81742ce2a1d4827d12db85a8d7f0dc34215cf
Gusztáv Szikszai <guszti5@hotmail.com>
2019-11-14 13:09:51 +0100

    Add throttle to docs search.

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -1,25 +1,29 @@
 const search = document.querySelector('[data-id=version-search]')
 const sidebar = document.querySelector('#sidebar')
+let id = null
 
 if (search) {
-  search.addEventListener('input', async () => {
-    const a = document.createElement('a')
-    a.href = window.location.toString()
+  search.addEventListener('input', () => {
+    clearTimeout(id)
+    id = setTimeout(async () => {
+      const a = document.createElement('a')
+      a.href = window.location.toString()
 
-    if (search.value.trim() !== "") {
-      a.search = `search=${search.value}`
-    } else {
-      a.search = ''
-    }
+      if (search.value.trim() !== "") {
+        a.search = `search=${search.value}`
+      } else {
+        a.search = ''
+      }
 
-    const response = await fetch(a.href, {
-      headers: { "Accept": "application/javascript" }
-    })
+      const response = await fetch(a.href, {
+        headers: { "Accept": "application/javascript" }
+      })
 
-    const html = await response.text()
+      const html = await response.text()
 
-    sidebar.innerHTML = html
+      sidebar.innerHTML = html
 
-    history.replaceState({}, document.title, a.href)
+      history.replaceState({}, document.title, a.href)
+    }, 300)
   })
 }
