6228ede07255a5530cc77d4719fd70ddc06fe8b4
Gusztáv Szikszai <guszti5@hotmail.com>
2019-11-28 06:15:10 +0100

    Added guide index page.

--- app/helpers/guides_helper.rb
+++ app/helpers/guides_helper.rb
@@ -1,5 +1,5 @@
 module GuidesHelper
   def toc_link(name, page)
-    active_link_to name, "/guide/#{page}", class: :toc__page, active: :exact
+    active_link_to name, "/guide#{page}", class: :toc__page, active: :exact
   end
 end
