fc454541639a51c328b015190d7249ee7accd7e1
Gusztáv Szikszai <guszti5@hotmail.com>
2020-01-02 17:03:31 +0100

    Added step navigation to the guide.

--- app/controllers/guides_controller.rb
+++ app/controllers/guides_controller.rb
@@ -1,7 +1,102 @@
 class GuidesController < ApplicationController
+  PAGES = [
+    {
+      title: 'Into',
+      pages: [
+        { title: "What & Why", path: "" }
+      ],
+    },
+    {
+      title: 'Getting Started',
+      pages: [
+        { title: "Tools", path: "/getting-started/tools" },
+        { title: "Using the CLI", path: "/getting-started/using-the-cli" },
+        { title: "Configuration", path: "/getting-started/configuration" },
+        { title: "The Main Component", path: "/getting-started/the-main-component" },
+        { title: "Styling with CSS", path: "/getting-started/styling-with-css" },
+        { title: "Handling Events", path: "/getting-started/handling-events" },
+      ]
+    },
+    {
+      title: 'Recipies',
+      pages: [
+        { title: "Using thirdy party CSS", path: "/recipies/using-third-party-css" }
+      ]
+    },
+      title: 'Reference',
+      pages: [
+        { title: "Literals", path: "/reference/literals" },
+        { title: "Functions", path: "/reference/functions" },
+        { title: "Records", path: "/reference/records" },
+        { title: "Enums", path: "/reference/enums" },
+        { title: "Built in Types", path: "/reference/built-in-types" },
+        { title: "Equality", path: "/reference/equality" },
+        { title: "Stores", path: "/reference/stores" },
+        { title: "Routing", path: "/reference/routing" },
+        {
+          title: "Control Expressions",
+          path: "/reference/control-expressions",
+          pages: [
+            { title: "if...else", path: "/reference/control-expressions/if-else" },
+            { title: "case", path: "/reference/control-expressions/case" },
+            { title: "for", path: "/reference/control-expressions/for" },
+            { title: "try", path: "/reference/control-expressions/try" },
+            { title: "sequence", path: "/reference/control-expressions/sequence" },
+            { title: "parallel", path: "/reference/control-expressions/parallel" },
+          ]
+        },
+        {
+          title: "Components",
+          path: "/reference/components",
+          pages: [
+            { title: "Properties", path: "/reference/components/properties" },
+            { title: "Computed Properties", path: "/reference/components/computed-properties" },
+            { title: "Styling Elements", path: "/reference/components/styling-elements" },
+            { title: "Connecting Stores", path: "/reference/components/connecting-stores" },
+            { title: "Using Providers", path: "/reference/components/using-providers" },
+            { title: "Internal State", path: "/reference/components/internal-state" },
+            { title: "Referencing Entities", path: "/reference/components/referencing-entities" },
+            { title: "Global Components", path: "/reference/components/global-components" },
+          ]
+        },
+        {
+          title: "JavaScript Interop",
+          path: "/reference/javascript-interop",
+          pages: [
+            { title: "Inlining", path: "/reference/javascript-interop/inlining" },
+            { title: "Decode Expression", path: "/reference/javascript-interop/decode-expression" },
+            { title: "Encode Expression", path: "/reference/javascript-interop/encode-expression" },
+          ]
+        },
+        { title: "Environment Variables", path: "/reference/environment-variables", pages: [] },
+        { title: "Packages", path: "/reference/packages" },
+      ]
+  ]
+
+  FLATTENED_PAGES =
+    PAGES.reduce([]) do |memo, page|
+      page[:pages].to_a.each do |subpage|
+        memo << subpage
+
+        subpage[:pages].to_a.each do |item|
+          memo << item
+        end
+      end
+
+      memo
+    end
+
   def page
     path =
       File.join(Rails.root, 'app', 'views', 'guides', 'pages', params[:page] + ".haml")
+
+    index =
+      FLATTENED_PAGES.find_index { |item| item[:path] == "/" + params[:page] }
+
+    if index
+      @next = FLATTENED_PAGES[index + 1]
+      @previous = FLATTENED_PAGES[index - 1]
+    end
 
     if File.exist?(path)
       @page = 'guides/pages/' + params[:page]
@@ -15,6 +110,7 @@ class GuidesController < ApplicationController
         redirect_to '/guide'
       end
     end
+
   end
 
   def index
--- app/helpers/guides_helper.rb
+++ app/helpers/guides_helper.rb
@@ -1,4 +1,36 @@
 module GuidesHelper
+  def pages
+    GuidesController::PAGES
+  end
+
+  def guide_nav_previous(path, title)
+    link_to "/guide#{path}", class: 'guide__nav__button' do
+      octicon("chevron-left", width: 12) +
+      tag.div do
+        tag.label("Previous") +
+        tag.span(title)
+      end
+    end
+  end
+
+  def guide_nav_next(path, title)
+    link_to "/guide#{path}", class: 'guide__nav__button' do
+      tag.div do
+        tag.label("Next") +
+        tag.span(title)
+      end +
+      octicon("chevron-right", width: 12)
+    end
+  end
+
+  def guide_nav
+    tag.div class: 'guide__nav' do
+      capture_haml do
+        yield
+      end
+    end
+  end
+
   def toc_link(name, page)
     active_link_to "/guide#{page}", class: :toc__page, active: :exact do
       tag.div name
