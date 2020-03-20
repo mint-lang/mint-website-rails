81dd4db4de197088059051ceb331abdf3d43bded
Gusztáv Szikszai <guszti5@hotmail.com>
2020-03-20 13:18:49 +0100

    Set protocol.

--- config/initializers/host.rb
+++ config/initializers/host.rb
@@ -1,3 +1,4 @@
 # frozen_string_literal: true
 
+Rails.application.routes.default_url_options[:protocol] = ENV['RAILS_HOST_PROTOCOL']
 Rails.application.routes.default_url_options[:host] = ENV['RAILS_HOST']
