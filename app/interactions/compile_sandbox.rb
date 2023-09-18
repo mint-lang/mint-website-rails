2c2f27b69f4d0e20457f5ab863b128aeb1a89643
Szikszai Gusztáv <guszti5@hotmail.com>
2023-09-18 12:13:13 +0200

    Fix syntax error.

--- app/interactions/compile_sandbox.rb
+++ app/interactions/compile_sandbox.rb
@@ -17,7 +17,7 @@ class CompileSandbox < ActiveInteraction::Base
       {
         "0.16.1" => 'https://mint-sandbox-compiler.herokuapp.com/compile',
         "0.17.0" => 'https://mint-sandbox-0170.szikszai.co/compile',
-        "0.18.0" => 'https://mint-sandbox-0180.szikszai.co/compile'
+        "0.18.0" => 'https://mint-sandbox-0180.szikszai.co/compile',
         "0.19.0" => 'https://mint-sandbox-0190.szikszai.co/compile'
       }
 
