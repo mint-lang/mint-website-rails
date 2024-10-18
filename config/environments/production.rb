7f8ba28343aa4675308643606427a44a8c1192a7
Szikszai Gusztáv <guszti5@hotmail.com>
2024-10-18 13:51:02 +0200

    Update production.rb

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -97,4 +97,6 @@ Rails.application.configure do
   # config.active_record.database_selector = { delay: 2.seconds }
   # config.active_record.database_resolver = ActiveRecord::Middleware::DatabaseSelector::Resolver
   # config.active_record.database_resolver_context = ActiveRecord::Middleware::DatabaseSelector::Resolver::Session
+
+   config.session_store(:cookie_store, key: '_mint_website_backend_session', secure: Rails.env.production?, domain: :all)
 end
